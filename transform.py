a44b78d567b4cf1638b4ee37dca1ddc8b9a58bf0
Jeet Patel <52279688+jitupatel2506@users.noreply.github.com>
2025-09-01 02:04:04 +0530

    Update transform.py

--- transform.py
+++ transform.py
@@ -186,8 +186,8 @@ def normalize_match(m, idx, channel_number=600):
         return None
 
     # Proxy wrap if fancode
-      if "fancode.com" in stream_url and not stream_url.startswith("https://mini.allinonereborn.online/events/stream_proxy.php?url=")
-          stream_url = "https://mini.allinonereborn.online/events/stream_proxy.php?url=" + stream_url
+    if "fancode.com" in stream_url and not stream_url.startswith("https://mini.allinonereborn.online/events/stream_proxy.php?url="):
+        stream_url = "https://mini.allinonereborn.online/events/stream_proxy.php?url=" + stream_url
                    
     # Detect language
     lang = detect_language_from_url(stream_url)
@@ -316,6 +316,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
