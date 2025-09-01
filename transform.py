66e32771dc1923be7395157a86c4f201084f06f8
Jeet Patel <52279688+jitupatel2506@users.noreply.github.com>
2025-09-01 15:04:02 +0530

    Update transform.py

--- transform.py
+++ transform.py
@@ -209,7 +209,7 @@ def normalize_match(m, idx, channel_number=600):
     thumbnail = (
         m.get("src")
         or m.get("image")
-        or "https://gitlab.com/appzombies/ipl_data_api/-/raw/main/cricket_league_vectors/all_live_streaming.png"
+        or "https://gitlab.com/ranginfotech89/ipl_data_api/-/raw/main/stream_categories/cricket_league_vectors/all_live_streaming_inonly.png"
     )
     return {
         "channelNumber": channel_num,
@@ -312,6 +312,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
