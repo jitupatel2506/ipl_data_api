0f01f0fb1948e41ef435b5d465f0b2ad2e9d2660
Jeet Patel <52279688+jitupatel2506@users.noreply.github.com>
2025-09-02 18:21:21 +0530

    Update ww_transform.py

--- ww_transform.py
+++ ww_transform.py
@@ -188,6 +188,9 @@ def normalize_match(m, idx, channel_number=600):
     # Proxy wrap if fancode
     if "fdlive.fancode.com" in stream_url and not stream_url.startswith("https://mini.allinonereborn.online/events/stream_proxy.php?url="):
         stream_url = "https://mini.allinonereborn.online/events/stream_proxy.php?url=" + stream_url
+
+    if "sonypartnersdaimenew.akamaized.net" in stream_url and not stream_url.startswith("https://mini.allinonereborn.online/events/stream_proxy.php?url="):
+        stream_url = "https://mini.allinonereborn.online/events/stream_proxy.php?url=" + stream_url
                    
     # Detect language
     lang = detect_language_from_url(stream_url)
