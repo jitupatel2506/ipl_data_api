9501ea94aa07431f56b566196e228016b40203d2
Jeet Patel <52279688+jitupatel2506@users.noreply.github.com>
2025-12-08 14:28:52 +0530

    Update fb_ww_transform.py

--- fb_ww_transform.py
+++ fb_ww_transform.py
@@ -11,7 +11,7 @@ from datetime import datetime  # ✅ Added for time formatting
 OUTPUT_FILE = "live_stream/auto_worldwide_football.json"
 MANUAL_FILE = "https://gitlab.com/ranginfotech89/ipl_data_api/-/raw/main/stream_categories/live_stream/football_streaming.json"
 # Local filenames (CI will download these via curl)
-LOCAL_FILES = ["football1.json", "football2.json"]
+LOCAL_FILES = ["football1.json", "football2.json", "football3.json"]
 CRICHD_SELECTED_URL = "https://gitlab.com/ranginfotech89/ipl_data_api/-/raw/main/stream_categories/live_stream/football_streaming.json"
 # Remote fallback URLs (used only if local files missing)
 FANCODE_URLS = [
